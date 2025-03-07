ea3fa1a77251b3d9c62948af33ac4d7c88721144
gopak-barvinok <polpot09090@proton.me>
2025-03-07 14:46:33 +0900

    remake router links

--- src/layouts/components/NavbarLinks.vue
+++ src/layouts/components/NavbarLinks.vue
@@ -1,20 +1,44 @@
+<script lang="ts" setup>
+import { RouterLink } from 'vue-router';
+import type {
+  NavGroup,
+  NavLink,
+  NavSectionTitle,
+  VerticalNavItems,
+} from '../types';
+import { useBlockchain } from '@/stores';
+
+const bch = useBlockchain();
+
+function isNavLink(nav: VerticalNavItems | any): nav is NavLink {
+  return (<NavLink>nav).to !== undefined;
+}
+
+</script>
+
 <template>
   <div class="join">
-    <button class="btn rounded-none cosmos-styles skew-x-[-35deg] transform normal-case">
+    <RouterLink class="btn rounded-none cosmos-styles skew-x-[-35deg] transform normal-case"
+    to="/cosmos"
+    >
       <span class="skew-x-[35deg] transform ">
         _Dashboard
       </span>
-    </button>
-    <button class="btn rounded-none cosmos-styles skew-x-[-35deg] transform normal-case">
+    </RouterLink>
+    <RouterLink class="btn rounded-none cosmos-styles skew-x-[-35deg] transform normal-case"
+    
+    >
       <span class="skew-x-[35deg] transform ">
         _Stacking
       </span>
-    </button>
-    <button class="btn rounded-none cosmos-styles skew-x-[-35deg] transform normal-case">
+    </RouterLink>
+    <RouterLink class="btn rounded-none cosmos-styles skew-x-[-35deg] transform normal-case"
+    
+    >
       <span class="skew-x-[35deg] transform ">
         _Governance
       </span>
-    </button>
+    </RouterLink>
   </div>
 </template>
 
