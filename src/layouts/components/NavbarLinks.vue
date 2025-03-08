b928d352442d5ec5cd45da7515c4e377b774990a
gopak-barvinok <polpot09090@proton.me>
2025-03-08 14:31:03 +0900

    added navbar links reload

--- src/layouts/components/NavbarLinks.vue
+++ src/layouts/components/NavbarLinks.vue
@@ -1,20 +1,36 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
+</script>
+
 <template>
   <div class="join">
-    <button class="btn rounded-none cosmos-styles skew-x-[-35deg] transform normal-case">
+    <RouterLink to="/cosmos" @click.native="reloadPage" class="btn rounded-none cosmos-styles skew-x-[-35deg] transform normal-case">
       <span class="skew-x-[35deg] transform ">
         _Dashboard
       </span>
-    </button>
-    <button class="btn rounded-none cosmos-styles skew-x-[-35deg] transform normal-case">
+    </RouterLink>
+    <RouterLink to="/cosmos/stacking" @click.native="reloadPage" class="btn rounded-none cosmos-styles skew-x-[-35deg] transform normal-case">
       <span class="skew-x-[35deg] transform ">
         _Stacking
       </span>
-    </button>
-    <button class="btn rounded-none cosmos-styles skew-x-[-35deg] transform normal-case">
+    </RouterLink>
+    <RouterLink to="/cosmos/gov" @click.native="reloadPage" class="btn rounded-none cosmos-styles skew-x-[-35deg] transform normal-case">
       <span class="skew-x-[35deg] transform ">
         _Governance
       </span>
-    </button>
+    </RouterLink>
   </div>
 </template>
 
+<script lang="ts">
+export default {
+  methods: {
+    reloadPage() {
+      setTimeout(() => {
+        window.location.reload();
+      }, 100); 
+    }
+  }
+};
+</script>
